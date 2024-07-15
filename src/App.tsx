dfacf7ec9c5150584dfdf6bc688169556857a470
Jorik van Ruiswijk <jorikvanruiswijk@gmail.com>
2024-07-15 11:51:47 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <>
+      <h1>Nice books huh!</h1>
+    </>
+  );
+};
+
+export default App;
